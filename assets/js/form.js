8889ea04340d9e249bcb582ad2995c9c61466881
MaxLC00 <167907843+MaxLC00@users.noreply.github.com>
2024-06-23 17:26:15 -0500

    add html/css for pages

---
+++ assets/js/form.js
@@ -0,0 +1,9 @@
+const username = document.getElementById('userName');
+const title = document.getElementById('title');
+const content = document.getElementById('content');
+const form = document.querySelector('.whole-form');
+
+form.addEventListener('click', function(event){
+    const new
+
+});
