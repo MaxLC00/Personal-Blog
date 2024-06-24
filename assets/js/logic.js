3e7abf5edce5208cf7786a8b10532ac4717d9359
MaxLC00 <167907843+MaxLC00@users.noreply.github.com>
2024-06-24 18:25:31 -0500

    Fixed for loop errors

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,37 +3,36 @@ const buttonEl = document.querySelectorAll('.button'); //selects only button cla
 const textboxEl = document.querySelectorAll('.tbox'); //selects only textbox class elements and creates array of them
 const bodyEl = document.body; //selects the body and its children
 
-console.log(buttonEl);
-console.log(textboxEl);
-
 let theme = 'light'; //defines a 'theme' to the page to mark whether page is light or dark mode, starting in light
 
-themeSwitcher.addEventListener('click', function() {
+themeSwitcher.addEventListener('click', function () {
     if (theme === 'light') { //checks pages theme, runs if the theme is light
         theme = 'dark'; //toggles page theme
         console.log("mode is dark");
         bodyEl.setAttribute('style', 'background-color: rgb(0, 0, 73); color: white;');
-        for (let i = 0; buttonEl.length; i++) { //moves through the array of buttons
+        for (let i = 0; i < buttonEl.length; i++) { //moves through the array of buttons
             buttonEl[i].setAttribute( // sets style attribute to each element in the array
-                'style', 
+                'style',
                 'background-color: rgb(206, 224, 248); color: black;');
-            textboxEl[i].setAttribute(
-                'style', 
-                'background-color: rgb(210, 210, 210);');
         }
-         //switches background to dark and elements to corresponding variants
+        for (let i = 0; i < textboxEl.length; i++) {
+            textboxEl[i].setAttribute(
+                'style',
+                'background-color: rgb(210, 210, 210);');
+            }//switches background to dark and elements to corresponding variants
     } else { // if theme is not light (aka dark) runs instead
-        theme = 'light'; // toggles page theme
+        theme = 'light';
         console.log("mode is light");
         bodyEl.setAttribute('style', 'background-color: rgb(206, 224, 248); color: black;');
-        for (let i = 0; buttonEl.length; i++) {
+        for (let i = 0; i < buttonEl.length; i++) {
             buttonEl[i].setAttribute(
-                'style', 
+                'style',
                 'background-color: rgb(0, 0, 73); color: white;');
+        }
+        for (let i = 0; i < textboxEl.length; i++) {
             textboxEl[i].setAttribute(
-                'style', 
+                'style',
                 'background-color: white;');
         }
-         //switches background to light and elements to corresponding variants
     }
-});
+    });
